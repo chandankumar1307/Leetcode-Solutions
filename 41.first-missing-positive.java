0b547f39a02cba3467bcda199796db72d7c2c00d
Chandan Kumar Saha <chandankumarshah8@gmail.com>
2022-06-07 22:54:14 +0530

    one more question done

---
+++ 41.first-missing-positive.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=41 lang=java
+ *
+ * [41] First Missing Positive
+ */
+
+// @lc code=start
+import java.util.*;
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int a = 0, i = 0;
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                a = nums[i];
+                break;
+            }
+        }
+
+        if (a > 1 || a == 0) {
+
+            return 1;
+        }
+        while (a == nums[i]) {
+            i++;
+            if (i == nums.length) {
+                a++;
+                return a;
+            }
+            if (a == nums[i])
+                continue;
+            a++;
+        }
+
+        return a;
+
+    }
+}
+// @lc code=end
