c0965d5ffbb4475e084702816f270cfa26b979db
Chandan Kumar Saha <chandankumarshah8@gmail.com>
2023-01-11 21:21:48 +0530

    283.move-zeros

---
+++ 283.move-zeroes.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=283 lang=java
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+import java.util.*;
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int c = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int t = nums[i];
+                nums[i] = nums[c];
+                nums[c] = t;
+                c++;
+            }
+
+        }
+
+    }
+}
+// @lc code=end
