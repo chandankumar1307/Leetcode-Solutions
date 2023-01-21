0810071669195afdfebc4c1094bf62c46145bdb0
Chandan Kumar Saha <chandankumarshah8@gmail.com>
2023-01-21 18:07:44 +0530

    48.rotate-image

---
+++ 48.rotate-image.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=48 lang=java
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        int temp = 0;
+        // transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        temp = 0;
+        // reverse each row of the transposed matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
+// @lc code=end
