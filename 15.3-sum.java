674f05c9f32a12d988df460fe5fd598b6593408e
Chandan Kumar Saha <chandankumarshah8@gmail.com>
2023-01-23 21:04:22 +0530

    15.3-sum

--- 15.3-sum.java
+++ 15.3-sum.java
@@ -5,38 +5,28 @@
  */
 
 // @lc code=start
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> l = new HashSet<>();
+        int n = nums.length;
+        if (n == 0)
+            return new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> l1 = new ArrayList<>();
-
-        if (nums.length < 3)
-            return list;
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            int p1 = i + 1;
-            int p2 = nums.length - 1;
-            while (p1 < p2) {
-                if ((nums[i] + nums[p1] + nums[p2]) == 0) {
-                    l1.add(nums[i]);
-                    l1.add(nums[p1]);
-                    l1.add(nums[p2]);
-                    list.add(l1);
-                    l1.clear();
-                    break;
-                } else if ((nums[p1] + nums[p2]) > nums[i])
-                    p2--;
-                else
-                    p1++;
+        int sum = 0;
+        for (int i = 0; i < n - 2; i++) {
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0)
+                    l.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+                if (sum > 0)
+                    k--;
+                if (sum < 0)
+                    j++;
             }
-
         }
-
-        return list;
-
+        return new ArrayList<>(l);
     }
 }
 // @lc code=end
